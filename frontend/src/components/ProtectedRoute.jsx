86ae4eb5ed52818a96284a1ff10870208178ca91
Austinkuria <kuriaaustin125@gmail.com>
2025-02-06 14:05:34 +0300

    Implement ProtectedRoute component with token expiration check and remove old ProtectedRoutes component

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,30 @@
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import jwt_decode from 'jwt-decode';
+
+const ProtectedRoute = ({ children }) => {
+    const token = localStorage.getItem('token');
+
+    // Function to check if the token is expired
+    const isTokenExpired = (token) => {
+        if (!token) return true;
+        const decodedToken = jwt_decode(token);
+        const currentTime = Date.now() / 1000; // Convert to seconds
+        return decodedToken.exp < currentTime;
+    };
+
+    // If the token doesn't exist or is expired, redirect to login
+    if (!token || isTokenExpired(token)) {
+        localStorage.removeItem('token');  // Remove expired token
+        return <Navigate to="/auth/login" />;
+    }
+
+    // If token exists and is not expired, render the children components
+    return children;
+};
+
+ProtectedRoute.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default ProtectedRoute;
