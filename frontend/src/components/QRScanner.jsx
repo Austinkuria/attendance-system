8be19d4608f21a06b149cb14ec699c484cd447b5
Austinkuria <kuriaaustin125@gmail.com>
2025-02-07 16:25:19 +0300

    Replace qrcode.react with qr-scanner library, update QRScanner component, and add styling for improved user experience

--- frontend/src/components/QRScanner.jsx
+++ frontend/src/components/QRScanner.jsx
@@ -1,9 +1,104 @@
+import { useEffect, useRef, useState } from "react";
+import "./QrStyles.css";
+import QrScanner from "qr-scanner";
 
 const QRScanner = () => {
+  // QR States
+  const scanner = useRef();
+  const videoEl = useRef(null);
+  const qrBoxEl = useRef(null);
+  const [qrOn, setQrOn] = useState(true);
+
+  // Result
+  const [scannedResult, setScannedResult] = useState("");
+
+  // Success
+  const onScanSuccess = (result) => {
+    // 🖨 Print the "result" to browser console.
+    console.log(result);
+    // ✅ Handle success.
+    // 😎 You can do whatever you want with the scanned result.
+    setScannedResult(result?.data);
+  };
+
+  // Fail
+  const onScanFail = (err) => {
+    // 🖨 Print the "err" to browser console.
+    console.log(err);
+  };
+
+  useEffect(() => {
+    const videoElement = videoEl.current;
+    if (videoElement && !scanner.current) {
+      // 👉 Instantiate the QR Scanner
+      scanner.current = new QrScanner(videoElement, onScanSuccess, {
+        onDecodeError: onScanFail,
+        // 📷 This is the camera facing mode. In mobile devices, "environment" means back camera and "user" means front camera.
+        preferredCamera: "environment",
+        // 🖼 This will help us position our "QrFrame.svg" so that user can only scan when qr code is put in between our QrFrame.svg.
+        highlightScanRegion: true,
+        // 🔥 This will produce a yellow (default color) outline around the qr code that we scan, showing a proof that our qr-scanner is scanning that qr code.
+        highlightCodeOutline: true,
+        // 📦 A custom div which will pair with "highlightScanRegion" option above 👆. This gives us full control over our scan region.
+        overlay: qrBoxEl?.current || undefined,
+      });
+
+      // 🚀 Start QR Scanner
+      scanner?.current
+        ?.start()
+        .then(() => setQrOn(true))
+        .catch((err) => {
+          if (err) setQrOn(false);
+        });
+    }
+
+    // 🧹 Clean up on unmount.
+    // 🚨 This removes the QR Scanner from rendering and using camera when it is closed or removed from the UI.
+      if (!videoElement) {
+      if (!videoEl?.current) {
+        scanner?.current?.stop();
+      }
+    };
+  }, []);
+
+  // ❌ If "camera" is not allowed in browser permissions, show an alert.
+  useEffect(() => {
+    if (!qrOn)
+      alert(
+        "Camera is blocked or not accessible. Please allow camera in your browser permissions and Reload."
+      );
+  }, [qrOn]);
+
   return (
-    <div>
-      <h3>QR Scanner</h3>
-      {/* QR scanner component code goes here */}
+    <div className="qr-reader">
+      {/* QR */}
+      <video ref={videoEl}></video>
+      <div ref={qrBoxEl} className="qr-box">
+        {!videoEl?.current && (
+          <img
+            src="/static/images/icons/scan_qr1.svg"
+            alt="Qr Frame"
+            width={256}
+            height={256}
+            className="qr-frame"
+          />
+        )}
+      </div>
+
+      {/* Show Data Result if scan is success */}
+      {scannedResult && (
+        <p
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: 99999,
+            color: "white",
+          }}
+        >
+          Scanned Result: {scannedResult}
+        </p>
+      )}
     </div>
   );
 };
