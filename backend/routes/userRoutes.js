2f420be1e5cb5995fffa5e67863f50f39917853c
Austinkuria <kuriaaustin125@gmail.com>
2025-02-09 21:04:41 +0300

    Fix formatting in userRoutes.js by correcting import statement alignment

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,8 +3,8 @@ const { check } = require('express-validator');
 const { 
   registerUser, 
   login,
-  signup
-  getUserProfile, 
+  signup,
+  getUserProfile,
   updateUserProfile, 
   getStudents,
   updateStudent,
