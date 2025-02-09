5fef69256722ec71eda91b4c2a265dc7f67c93e2
Austinkuria <kuriaaustin125@gmail.com>
2025-02-09 21:26:39 +0300

    Add new routes for authentication, departments, courses, units, attendance, and sessions; update student upload routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,50 @@
 const express = require('express');
 const userRoutes = require('./userRoutes');
 const studentRoutes = require('./studentRoutes');
-
+const authenticate = require("../middleware/authMiddleware");
+const authorize = require("../middleware/authorizeMiddleware");
+const upload = require("../middleware/uploadMiddleware");
+const departmentRoutes = require("../routes/departmentRoutes");
+const courseRoutes = require("../routes/courseRoutes");
+const unitRoutes = require("../routes/unitRoutes");
+const attendanceRoutes = require("../routes/attendance.routes");
+const sessionRoutes = require("./sessionRoutes");
 const router = express.Router();
 
 router.use('/', userRoutes);
 router.use('/students', studentRoutes);
 
+// lecturer routes
+router.use('/')
+router.post("/auth/signup",userRoutes);
+router.post("/auth/login", userRoutes);
+
+// Department routes (admin only)
+router.use("/department", departmentRoutes);  // Register department routes under /department
+
+// Course routes (admin only)
+router.use("/course", courseRoutes);
+
+// Unit routes
+router.use("/unit", unitRoutes);
+
+// Attendance routes
+router.use("/attendance/", authenticate, attendanceRoutes);
+
+// Session routes
+router.use("/sessions", authenticate, sessionRoutes);
+
+
+// Define the  route for lecture
+router.get('/lecturers', userRoutes);
+
+// Admin routes (admin only)
+router.post("/user", authenticate, authorize(["admin"]), userRoutes);
+router.post("/upload-students", authenticate, authorize(["admin"]), upload.single("csvFile"), studentRoutes);
+router.post("/upload", upload.single("csvFile"),studentRoutes);
+router.get("/download",studentRoutes);
+router.delete("/students/:id",studentRoutes);
+
+// Update import route to use correct path
+router.post('/students/upload', upload.single("csvFile"), studentRoutes);
 module.exports = router;
