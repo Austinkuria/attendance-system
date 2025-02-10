26fc2f2c4bb3a577d71f7c4ae82c7d535fcdc45b
Austinkuria <kuriaaustin125@gmail.com>
2025-02-10 22:51:47 +0300

    Refactor quiz routes to simplify endpoint paths for adding and retrieving quizzes

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -4,9 +4,8 @@ const authenticate = require('../middleware/authMiddleware');
 const router = express.Router();
 
 // Route to add a new quiz
-router.post('/quizzes', authenticate, quizController.addQuiz);
+router.post('/', authenticate, quizController.addQuiz);
 
 // Route to retrieve all quizzes
-router.get('/quizzes', authenticate, quizController.getQuizzes);
-
+router.get('/', authenticate, quizController.getQuizzes);
 module.exports = router;
