045134cfa525d5da8e565ffcbd3cf5317a61b32c
Austinkuria <kuriaaustin125@gmail.com>
2025-02-11 01:15:45 +0300

    Refactor quiz routes: rename getQuizzes to getPastQuizzes for clarity

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -7,5 +7,5 @@ const router = express.Router();
 router.post('/', authenticate, quizController.addQuiz);
 
 // Route to retrieve all quizzes
-router.get('/', authenticate, quizController.getQuizzes);
+router.get('/', authenticate, quizController.getPastQuizzes);
 module.exports = router;
