fe64528cbefcc117562e005f1e71d63fe44ccab5
Austinkuria <kuriaaustin125@gmail.com>
2025-02-12 01:12:24 +0300

    Add delete quiz route: implement endpoint to allow deletion of quizzes

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -8,4 +8,5 @@ router.post('/', authenticate, quizController.addQuiz);
 
 // Route to retrieve all quizzes
 router.get('/', authenticate, quizController.getPastQuizzes);
+router.delete('/:id', authenticate, quizController.deleteQuiz); // Route to delete a quiz
 module.exports = router;
