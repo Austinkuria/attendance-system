a66411b134fc06701bbced0670dca7df2c6424d7
Austinkuria <kuriaaustin125@gmail.com>
2025-02-13 20:59:25 +0300

    Add quiz retrieval routes and initialize package.json and package-lock.json

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post('/', authenticate, quizController.addQuiz);
 // router.get('/:sessionId', authenticate, quizController.getQuizzesForSession);
 // router.post('/submit', authenticate, quizController.submitQuiz);
-
+// Route to retrieve all quizzes
+router.get('/', authenticate, quizController.getQuizzes);
+router.get('/', authenticate, quizController.getPastQuizzes);
 module.exports = router;
