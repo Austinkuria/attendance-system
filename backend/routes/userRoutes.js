c44cc7f89967d2b80e3863c9387a530f3c5975c2
Austinkuria <kuriaaustin125@gmail.com>
2025-02-17 01:14:56 +0300

    Refactor user routes to integrate registerUser middleware and enhance validation for signup

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { check } = require('express-validator');
 const {
   login,
-  signup,
+  // signup,
+  registerUser,
   getUserProfile,
   updateUserProfile,
   getStudents,
@@ -23,20 +24,20 @@ const authorize = require("../middleware/authorizeMiddleware");
 const upload = require("../middleware/uploadMiddleware");
 const router = express.Router();
 const quizController = require('../controllers/quizController');
-const { passwordValidation } = require("../validators/authValidation");
+// const { passwordValidation } = require("../validators/authValidation");
 
 // Auth routes
+router.post("/auth/login", [
+  check('email').isEmail().withMessage('Enter a valid email address'),
+  check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+], login);
+
 router.post("/auth/signup", [
   check('firstName').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long').matches(/^[A-Za-z]+$/).withMessage('First name must not contain numbers'),
   check('lastName').isLength({ min: 3 }).withMessage('Last name must be at least 3 characters long').matches(/^[A-Za-z]+$/).withMessage('Last name must not contain numbers'),
   check('email').isEmail().withMessage('Enter a valid email address'),
-  passwordValidation  // Use the password validation rule
-], signup);
-
-router.post("/auth/login", [
-  check('email').isEmail().withMessage('Enter a valid email address'),
-  passwordValidation  // Use the same password validation rule
-], login);
+  check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+], registerUser,signup);
 
 // Profile routes
 router.get("/users/profile", authenticate, getUserProfile);
