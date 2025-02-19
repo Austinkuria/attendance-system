0049c2bf4c53d564f001489f2b19603f5a879439
Austinkuria <kuriaaustin125@gmail.com>
2025-02-19 22:15:26 +0300

    Update service worker registration URL for consistency

--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -8,9 +8,7 @@ export function register(config) {
 
 
     window.addEventListener('load', () => {
-      const swUrl = `${import.meta.env.BASE_URL}/service-worker.js`;
-
-
+      const swUrl = '/sw.js';
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
       } else {
