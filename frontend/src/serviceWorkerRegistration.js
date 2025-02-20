0f87cb4184414fe54f19ed5fcb7b7860d2b5f852
Austinkuria <kuriaaustin125@gmail.com>
2025-02-20 20:25:57 +0300

    Update service worker registration URL to use 'sw.js' for improved clarity and consistency

--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -8,7 +8,7 @@ export function register(config) {
 
 
     window.addEventListener('load', () => {
-      const swUrl = `${import.meta.env.BASE_URL}/service-worker.js`;
+      const swUrl = `${import.meta.env.BASE_URL}/sw.js`;
 
 
       if (isLocalhost) {
