cfa780b709d069ac2342a49991f3f0cfe94bf660
Austinkuria <kuriaaustin125@gmail.com>
2025-02-25 00:16:32 +0300

    Reapplied local changes after sync

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-be69b4f6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cfq7212n4so"
+    "revision": "0.jujcttdkuv"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
