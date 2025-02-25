f201d19c84c1ded2486668e94ca19c9c306e86df
Austinkuria <kuriaaustin125@gmail.com>
2025-02-25 10:59:32 +0300

    Update .gitignore and service worker; remove package-lock.json files

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-be69b4f6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cum1d8osjl"
+    "revision": "0.pg77t1kclp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
